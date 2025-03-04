31960cce9957a301aaa1c9f25a87643a26f6151b
Greg Edelston <gredelston@gmail.com>
2025-03-04 08:03:28 -0700

    add script to interact with query server

---
+++ query.py
@@ -0,0 +1,35 @@
+#!/srv/minecraft/scripts/mcstatus.venv/bin/python3
+
+import argparse
+import enum
+
+import mcstatus
+
+class Command(enum.Enum):
+    PING = "ping"
+    STATUS = "status"
+    QUERY = "query"
+    WHO = "who"
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "command",
+    nargs="?",
+    choices=[cmd.value for cmd in Command],
+    default=Command.QUERY.value,
+)
+args = parser.parse_args()
+
+server = mcstatus.JavaServer("minecraft.gredelston.cool")
+
+match args.command:
+    case Command.PING.value:
+        print(server.ping())
+    case Command.STATUS.value:
+        print(server.status())
+    case Command.QUERY.value:
+        print(server.query().raw)
+    case Command.WHO.value:
+        print(server.query().players.names)
+    case _:
+        raise ValueError(args.command)
