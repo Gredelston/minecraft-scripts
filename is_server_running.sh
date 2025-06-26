50c7af05783e5e93caa70c88d115fb3a3c7a8a7b
Greg Edelston <gredelston@gmail.com>
2025-06-26 13:23:57 -0600

    add docstring to is_server_running.sh

--- is_server_running.sh
+++ is_server_running.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
+#
+# Check whether the Minecraft server is currently running.
+# Exits with status 0 if the server is running, 1 otherwise.
 
 /srv/minecraft/scripts/query.py status &> /dev/null
 exit $?
